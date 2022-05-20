9b53680ebe2f1a9f354a65dbfc9d0c7aac2d252a
Gustavo Duran Ferreira <gustavo.ferreira@philips.com>
2022-05-20 16:33:44 -0300

    Exercice-1 v2

--- test_hike_content.py
+++ test_hike_content.py
@@ -1,6 +1,4 @@
 import unittest
-from unittest.mock import patch
-from read_excel_isolated import read_hike_data
 
 class TestRead(unittest.TestCase):
 
@@ -9,13 +7,8 @@ class TestRead(unittest.TestCase):
     "hike": ['10%', '20%']
   }
 
-  def has_percentage_char(s):
-    s.count("%");
-
-  @patch('read_excel_isolated.pandas.read_excel', return_value=sample_emails)
   def test_is_percentage(self, _):
-    hikes = read_hike_data()
-    self.assertEqual(len(hikes), len(map(has_percentage_char, TestRead.sample_emails["hike"])))
+    self.assertEqual(true, all(list(map(lambda s: s.count('%') > 0, TestRead.sample_emails["hike"]))))
 
 if __name__ == '__main__':
   unittest.main()
