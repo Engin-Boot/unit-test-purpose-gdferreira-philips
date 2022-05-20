422d119dad10ccf3a64472802866940ed2ae06d5
Gustavo Duran Ferreira <gustavo.ferreira@philips.com>
2022-05-20 16:17:28 -0300

    Exercice-1

--- test_hike_content.py
+++ test_hike_content.py
@@ -1,10 +1,21 @@
 import unittest
-
+from unittest.mock import patch
+from read_excel_isolated import read_hike_data
 
 class TestRead(unittest.TestCase):
-    def test_enter_the_name_of_our_test_here(self):
-        self.assertFalse("test isn't implemented yet")
 
+  sample_emails = {
+    "email": ['h@dummy.com', 'q@dummy.com'],
+    "hike": ['10%', '20%']
+  }
+
+  def has_percentage_char(s):
+    s.count("%");
+
+  @patch('read_excel_isolated.pandas.read_excel', return_value=sample_emails)
+  def test_is_percentage(self, _):
+    hikes = read_hike_data()
+    self.assertEqual(len(hikes), len(map(has_percentage_char, TestRead.sample_emails["hike"])))
 
 if __name__ == '__main__':
   unittest.main()
